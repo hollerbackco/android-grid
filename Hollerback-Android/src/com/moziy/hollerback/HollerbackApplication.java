8e9689bd424250100aad6800dd27eb9ea4c3e4f8
stabib <sajjad@hollerback.co>
2013-11-18 12:31:23 -0800

    clear all transacting model on app launch

--- Hollerback-Android/src/com/moziy/hollerback/HollerbackApplication.java
+++ Hollerback-Android/src/com/moziy/hollerback/HollerbackApplication.java
@@ -1,28 +1,38 @@
 package com.moziy.hollerback;
 
+import android.util.Log;
+
 import com.activeandroid.ActiveAndroid;
+import com.activeandroid.query.Set;
+import com.activeandroid.query.Update;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.moziy.hollerback.database.ActiveRecordFields;
 import com.moziy.hollerback.lifecycle.AppLifecycle;
+import com.moziy.hollerback.model.VideoModel;
+import com.moziy.hollerback.service.task.ActiveAndroidUpdateTask;
+import com.moziy.hollerback.service.task.TaskExecuter;
 import com.moziy.hollerback.util.DataModelManager;
 
 public class HollerbackApplication extends com.activeandroid.app.Application {
     private static HollerbackApplication sInstance = null;
-
+    private static final String TAG = HollerbackApplication.class.getSimpleName();
     private static DataModelManager sDataModelManager = null;
     private ObjectMapper mObjectMapper;
     private AppLifecycle mLifecycle;
 
     @Override
     public void onCreate() {
-        // TODO Auto-generated method stub
         super.onCreate();
+
         ActiveAndroid.setLoggingEnabled(true);
         initObjectMapper();
 
         sDataModelManager = new DataModelManager();
         mLifecycle = new AppLifecycle();
+
+        clearAllTransactingModel();
 
     }
 
@@ -41,12 +51,6 @@ public class HollerbackApplication extends com.activeandroid.app.Application {
 
     public DataModelManager getDM() {
         return sDataModelManager;
-    }
-
-    @Override
-    public void onTerminate() {
-        // TODO Auto-generated method stub
-        super.onTerminate();
     }
 
     @Override
@@ -75,6 +79,20 @@ public class HollerbackApplication extends com.activeandroid.app.Application {
 
     public AppLifecycle getAppLifecycle() {
         return mLifecycle;
+    }
+
+    /**
+     * This method will clear all transacting model in the event the model
+     * was set to transacting and the app got killed
+     */
+    public void clearAllTransactingModel() {
+        // TODO - sajjad: needs to be tested
+        Log.d(TAG, "clearing all transacting model.");
+        Set updateStatement = new Update(VideoModel.class).set(ActiveRecordFields.C_VID_TRANSACTING + "=?", 0);
+        ActiveAndroidUpdateTask updateTask = new ActiveAndroidUpdateTask(updateStatement);
+        TaskExecuter exeucter = new TaskExecuter();
+        exeucter.executeTask(updateTask);
+
     }
 
 }
