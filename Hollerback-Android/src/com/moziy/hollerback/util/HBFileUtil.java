928e464abea8d27cce115137130867b91c628cff
stabib <sajjad@hollerback.co>
2013-12-11 10:39:07 -0800

    added .nomedia when creating dirs to prevent media files from showing up in gallery

--- Hollerback-Android/src/com/moziy/hollerback/util/HBFileUtil.java
+++ Hollerback-Android/src/com/moziy/hollerback/util/HBFileUtil.java
@@ -1,15 +1,13 @@
 package com.moziy.hollerback.util;
 
 import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.io.IOException;
 import java.util.Locale;
 import java.util.Random;
 import java.util.UUID;
 import java.util.regex.Matcher;
 
 import android.media.MediaRecorder.OutputFormat;
-import android.net.Uri;
 import android.os.Environment;
 import android.util.Log;
 
@@ -25,46 +23,6 @@ public class HBFileUtil {
     public static final int MEDIA_TYPE_IMAGE = 1;
     public static final int MEDIA_TYPE_VIDEO = 2;
 
-    @Deprecated
-    // not used
-    /** Create a file Uri for saving an image or video */
-    public static Uri getOutputMediaFileUri(int type) {
-        return Uri.fromFile(getOutputMediaFile(type));
-    }
-
-    @Deprecated
-    // not used
-    /** Create a File for saving an image or video */
-    public static File getOutputMediaFile(int type) {
-        // To be safe, you should check that the SDCard is mounted
-        // using Environment.getExternalStorageState() before doing this.
-
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES), DIRECTORY_NAME);
-        // This location works best if you want the created images to be shared
-        // between applications and persist after your app has been uninstalled.
-
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists()) {
-            if (!mediaStorageDir.mkdirs()) {
-                Log.d(TAG, "failed to create directory");
-                return null;
-            }
-        }
-
-        // Create a media file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        File mediaFile;
-        if (type == MEDIA_TYPE_IMAGE) {
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
-        } else if (type == MEDIA_TYPE_VIDEO) {
-            mediaFile = new File(mediaStorageDir.getPath() + File.separator + "VID_" + timeStamp + ".mp4");
-        } else {
-            return null;
-        }
-
-        return mediaFile;
-    }
-
     public static File getOutputVideoFile(VideoModel video) {
         String filename = video.getGuid();
 
@@ -76,6 +34,15 @@ public class HBFileUtil {
             if (!mediaStorageDir.mkdirs()) {
                 Log.d(TAG, "failed to create directory");
                 return null;
+            }
+
+            // create a no media file and place it
+            File nomedia = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + DIRECTORY_NAME + "/" + subDir + "/.nomedia");
+            try {
+                nomedia.createNewFile();
+            } catch (IOException e) {
+                Log.w(TAG, "couldn't create .nomedia file in dir: " + Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + DIRECTORY_NAME + "/" + subDir);
+                e.printStackTrace();
             }
         }
 
@@ -117,6 +84,15 @@ public class HBFileUtil {
             if (!mediaStorageDir.mkdirs()) {
                 Log.d(TAG, "failed to create directory");
                 return null;
+            }
+
+            // create a no media file and place it
+            File nomedia = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + DIRECTORY_NAME + "/" + fileParts[0] + "/.nomedia");
+            try {
+                nomedia.createNewFile();
+            } catch (IOException e) {
+                Log.w(TAG, "couldn't create .nomedia file in dir: " + Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + DIRECTORY_NAME + "/" + fileParts[0]);
+                e.printStackTrace();
             }
         }
         // Create a media file name
