433aac0e2034623ab978286a4819cbf6c27f412c
stabib <sajjad@hollerback.co>
2014-01-31 16:07:08 -0800

    draw tint over imageview when pressed

--- Hollerback-Android/src/com/moziy/hollerback/view/SquareImageView.java
+++ Hollerback-Android/src/com/moziy/hollerback/view/SquareImageView.java
@@ -1,14 +1,21 @@
 package com.moziy.hollerback.view;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.widget.ImageView;
+
+import com.moziy.hollerback.R;
 
 public class SquareImageView extends ImageView {
     private static final String TAG = SquareImageView.class.getSimpleName();
 
+    private boolean mDrawTint;
+    private int mTintColor;
+
     public SquareImageView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        mTintColor = getResources().getColor(R.color.gray_disabled);
     }
 
     public SquareImageView(Context context, AttributeSet attrs) {
@@ -35,4 +42,26 @@ public class SquareImageView extends ImageView {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
     }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (mDrawTint) {
+            canvas.drawColor(mTintColor);
+        }
+
+    }
+
+    @Override
+    protected void drawableStateChanged() {
+        super.drawableStateChanged();
+        if (isPressed()) {
+            mDrawTint = true;
+            invalidate();
+        } else {
+            mDrawTint = false;
+            invalidate();
+        }
+    }
+
 }
